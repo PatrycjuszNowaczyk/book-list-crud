889948591641a11ac83baedd6fa99591a6dad67a
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@provider.pl>
2024-04-18 22:31:49 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/book-list-crud/',
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  }
+})
