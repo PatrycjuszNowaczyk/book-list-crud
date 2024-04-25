7bf79d202b3f0349b67da77b3caad7e915aa50cf
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@provider.pl>
2024-04-25 09:56:29 +0200

    feat: remove autocomplete from input

--- src/components/AddBook/AddBookForm.jsx
+++ src/components/AddBook/AddBookForm.jsx
@@ -38,6 +38,7 @@ const AddBookForm = () => {
               sx={{
                 marginBottom: 2,
               }}
+              autoComplete='off'
             />
             
             <Button
